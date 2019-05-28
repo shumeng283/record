0af7fccd573ed7ed0da7af96910ac27183865677
shumeng283 <527522643@qq.com>
2019-05-28 14:47:50 +0800

    xx

--- t-t3.py
+++ t-t3.py
@@ -1,5 +1,18 @@
 # Author :lixinhao
-from send_email import SendMail
+l = [1, 3, 5, 7, 0, -1, -9, -4, -5, 8]
+m = [i for i in l if i < 0]
+n = [i for i in l if i > 0]
+print('正数%s,负数%s' %(len(n),len(m)))
 
+s = 'axbyczdj'
+print(s[0::2])
 
+ss = 'hello_world_yoyo'
+print(ss.split('_'))
 
+a = 1
+print('%04d' %a)
+
+aa = [1,3,5,7]
+aa.insert(3,1)
+print(aa[1:])
--- t-t4.py
+++ t-t4.py
@@ -1,20 +1,28 @@
 # Author :lixinhao
 l = []
 for i in range(100,1000):
-    s = 0
+    sum = 0
     m = list(str(i))
     for j in m:
-        s += int(j) ** 3
-    if s == i:
-        l.append(i)
+        sum += int(j)**3
+        if sum == i:
+            l.append(i)
 print(l)
 
 ll = []
 for i in range(1,1000):
-    s = 0
+    sum = 0
     for j in range(1,i):
-        if i % j == 0 and i > j:
-            s += j
-    if s == i:
+        if i % j == 0 and i != j:
+            sum += j
+    if sum == i:
         ll.append(i)
 print(ll)
+
+a = [2,9,78,36,31,111,66]
+b = range(1,len(a))[::-1]
+for i in b:
+    for j in range(i):
+        if a[j] > a[j+1]:
+            a[j],a[j+1] = a[j+1],a[j]
+    print('第%s冒泡,排序为%s' %(len(a)-i,a))
